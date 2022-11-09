6679eaee611389e0dee32ef97bd5825c33f576f7
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2022-11-09 03:49:34 -0500

    Basic CRUD finished (example not implemented)

--- femme_back/user/urls.py
+++ femme_back/user/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.getRoute),
-    path('nombres/', views.getNombres)
+    path('nombres/', views.getNombres),
+    path('nombres/create/', views.createNombre),
+    path('nombres/<str:pk>/update', views.updateNombre),
+    path('nombres/<str:pk>/delete', views.deleteNombre),
+    path('nombres/<str:pk>/', views.getNombre),
+    
 ]
--- femme_back/user/views.py
+++ femme_back/user/views.py
@@ -23,10 +23,53 @@ def getRoute(request):
 
 
 """
-Serializamos para poder obtener la informacion en JSON's
+Serializamos para poder obtener la informacion de todas las personas
 """
 @api_view(['GET'])
 def getNombres(request):
     nombres = Nombre.objects.all()
     serializer = NombreSerializer(nombres, many=True)
     return  Response(serializer.data)
+
+"""
+Serializamos para poder obtener la informacion de una persona en especifico
+"""
+@api_view(['GET'])
+def getNombre(request, pk):
+    nombres = Nombre.objects.get(id=pk)
+    serializer = NombreSerializer(nombres, many=False)
+    return  Response(serializer.data)
+
+"""
+Serializamos para poder crear un nombre
+"""
+@api_view(['POST'])
+def createNombre(request):
+    data = request.data
+    nombre = Nombre.objects.create(
+        body = data['body']
+    )
+    serializer = NombreSerializer(nombre, many=False)
+    return Response(serializer.data)
+
+"""
+Serializamos para poder actualizar el contenido de una persona
+"""
+@api_view(['PUT'])
+def updateNombre(request, pk):
+    data = request.data
+    nombre = Nombre.objects.get(id=pk)
+
+    serializer = NombreSerializer(nombre, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+"""
+Eliminamos un nombre de la base de datos
+"""
+@api_view(['DEL'])
+def deleteNombre(request, pk):
+    nombre = Nombre.objects.get(id=pk)
+    nombre.delete()
+    return Response('Se elimino el nombre!')
