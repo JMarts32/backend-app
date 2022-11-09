ea066991e04ce0a527013f52106ce73359acdbc0
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2022-11-09 03:56:46 -0500

    Any connection available into the API

--- femme_back/femme_back/settings.py
+++ femme_back/femme_back/settings.py
@@ -40,10 +40,14 @@ INSTALLED_APPS = [
 
     'user.apps.UserConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +130,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
